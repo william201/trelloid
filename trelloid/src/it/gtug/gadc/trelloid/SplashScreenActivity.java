dbd70d5c2a9db4f05efdfdd05b8b571e9482a686
fabio.collini@gmail.com <fabio.collini@gmail.com@7f54800e-fe7e-e638-011d-09af164633d5>
2012-02-19 15:46:48 +0000

    Introdotto ActionbarSherlock

--- trelloid/src/it/gtug/gadc/trelloid/SplashScreenActivity.java
+++ trelloid/src/it/gtug/gadc/trelloid/SplashScreenActivity.java
@@ -1,15 +1,8 @@
 package it.gtug.gadc.trelloid;
 
-import it.gtug.gadc.trelloid.model.Board;
-import it.gtug.gadc.trelloid.model.Card;
-import it.gtug.gadc.trelloid.model.CardContainer;
-import it.gtug.gadc.trelloid.services.BoardService;
-import it.gtug.gadc.trelloid.services.MemberService;
-
 import java.util.Arrays;
 import java.util.List;
 
-import org.jboss.resteasy.client.ProxyFactory;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
@@ -28,41 +21,41 @@ import android.widget.ListView;
 public class SplashScreenActivity extends ListActivity {
 
 	private final static String key = "9bd5f87e01424e4cae086ea481513c86";
-	public final static String testKey = "9bd5f87e01424e4cae086ea481513c86";
-	public final static String testToken = "3b92413f53999b697d203734443e1c9af92be868a2be81ea541484db25aa4a22";
+	public final static String testKey = "1b59bc31a7420d12ce644d7d822161a2";
+	public final static String testToken = "a30c5f4656e4003a9a84c36a25fda4f8152d3069c39a356d915cb0dcbc094e72";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
 
-		MemberService service = ProxyFactory.create(MemberService.class,
-				"https://api.trello.com");
-		BoardService boardService = ProxyFactory.create(BoardService.class,
-				"https://api.trello.com");
-
-		List<Board> boards = service.listMemberBoards("fabriziooo", key);
-
-		for (Board board : boards) {
-			System.err.println("id: " + board.getId());
-			System.err.println("desc: " + board.getName());
-
-			List<CardContainer> listOfCardContainer = boardService
-					.findListsForBoard(board.getId(), key);
-
-			for (CardContainer cardContainer : listOfCardContainer) {
-				System.err.println("\tid: " + cardContainer.getId());
-				System.err.println("\tdesc: " + cardContainer.getName());
-
-				for (Card card : cardContainer.getCards()) {
-					System.err.println("\t\tid: " + card.getId());
-					System.err.println("\t\tdesc: " + card.getName());
-
-				}
-
-			}
-
-		}
+		// MemberService service = ProxyFactory.create(MemberService.class,
+		// "https://api.trello.com");
+		// BoardService boardService = ProxyFactory.create(BoardService.class,
+		// "https://api.trello.com");
+		//
+		// List<Board> boards = service.listMemberBoards("fabriziooo", key);
+		//
+		// for (Board board : boards) {
+		// System.err.println("id: " + board.getId());
+		// System.err.println("desc: " + board.getName());
+		//
+		// List<CardContainer> listOfCardContainer = boardService
+		// .findListsForBoard(board.getId(), key);
+		//
+		// for (CardContainer cardContainer : listOfCardContainer) {
+		// System.err.println("\tid: " + cardContainer.getId());
+		// System.err.println("\tdesc: " + cardContainer.getName());
+		//
+		// for (Card card : cardContainer.getCards()) {
+		// System.err.println("\t\tid: " + card.getId());
+		// System.err.println("\t\tdesc: " + card.getName());
+		//
+		// }
+		//
+		// }
+		//
+		// }
 
 		List<Class<?>> l = Arrays.<Class<?>> asList(BoardListActivity.class,
 				BoardActivity.class, CardActivity.class);
