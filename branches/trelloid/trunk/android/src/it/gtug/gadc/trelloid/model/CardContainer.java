a7b20b9106c8036cf8929103910a06c1aa79cec4
lucamasini <lucamasini@7f54800e-fe7e-e638-011d-09af164633d5>
2012-02-26 08:57:12 +0000

    Migrazione a Maven

---
+++ branches/trelloid/trunk/android/src/it/gtug/gadc/trelloid/model/CardContainer.java
@@ -0,0 +1,66 @@
+package it.gtug.gadc.trelloid.model;
+
+import it.gtug.gadc.trelloid.model.Card;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Questa classe rappresenta cio' che nel dominio applicativo si chiama LISTA
+ * (con all'interno le CARDS)
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CardContainer implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String idBoard;
+	private String id;
+	private String name;
+
+	private List<Card> cards;
+
+	public CardContainer() {
+
+	}
+
+	public CardContainer(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Card> getCards() {
+		return cards;
+	}
+
+	public void setCards(List<Card> cards) {
+		this.cards = cards;
+	}
+
+	public String getIdBoard() {
+		return idBoard;
+	}
+
+	public void setIdBoard(String idBoard) {
+		this.idBoard = idBoard;
+	}
+}
